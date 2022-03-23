8c11f443278815af3fd0ad9c6ea3a549d77fa5de
Gaein nidb <mail@gaein.cn>
2022-03-23 22:25:57 +0800

    Update home page

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,29 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+    <v-card :tile="true" class="overflow-hidden">
+      <NavBar />
+      <v-sheet
+          id="scrolling-techniques-2"
+          class="overflow-y-auto"
+          max-height="100vh"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+        <v-container style="padding-top: 128px; height: 3000px;">
+          <HelloWorld />
+        </v-container>
+      </v-sheet>
+    </v-card>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld';
+import NavBar from "@/components/NavBar";
 
 export default {
   name: 'App',
 
   components: {
+    NavBar,
     HelloWorld,
   },
 
---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app-bar
+      absolute
+      color="#fcb69f"
+      dark
+      shrink-on-scroll
+      src="@/assets/banner.png"
+      scroll-target="#scrolling-techniques-2"
+  >
+    <template v-slot:img="{ props }">
+      <v-img
+          v-bind="props"
+          gradient="to top right, rgba(135, 206, 235, 0.5), rgba(47, 79, 79, 0.8)"
+      ></v-img>
+    </template>
+
+    <h2>龙山Craft —— 中北人的MC</h2>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>mdi-gamepad-variant</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-github</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-information</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style scoped>
+
+</style>
