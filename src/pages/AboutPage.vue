26eea5f268bcf1a87f8cc3b724aefe4f0c09e1b1
Gaein nidb <mail@gaein.cn>
2022-03-25 23:18:56 +0800

    Update about page content

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title>关于服务器</v-card-title>
+      <v-card-subtitle>NUCraft</v-card-subtitle>
+      <v-card-text>
+        <p>龙山Craft
+          成立于2020年，最初为2020届中北C语音学习群基友服，后与校漫无止境动漫社的@TATA协商后共同成立龙山Craft，由AI+移动互联创新实验室@Gaein_nidb提供服务器、校园网线路以及运维。</p>
+        <p>服务器成立以来一直努力营造一个温馨和谐的氛围，版本升降、插件增删以及游戏内各事项均由玩家商议决定</p>
+        <p>两年时间中，广大玩家齐心协力，建造了龙山城等建筑，猪人塔以及农场等各种设施。动漫创新实验室的@Admiral_Spee_修建了数条铁路线路及沿线设施，极大的方便了玩家出行。</p>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "AboutPage"
+}
+</script>
