dea18ff110fcaafb94daafb877268a480f4f65b2
Gaein nidb <mail@gaein.cn>
2022-04-25 01:41:47 +0800

    Add version information

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,12 @@
           <router-view/>
         </v-container>
         <v-footer style="background-color: #287d2f" class="white--text">
-          <span>©2020-{{ new Date().getFullYear() }} 龙山市文化和旅游局</span>
+          <span class="mx-2">©2020-{{ new Date().getFullYear() }} 龙山市文化和旅游局</span>
+          <span class="mx-2">版本:
+            <a class="blue--text" target="_blank"
+               :href="`https://github.com/NUC-Craft/nucraft-web/commit/${versionInfo.COMMIT_HASH}`">
+            git-{{ versionInfo.BRANCH }}-{{ versionInfo.VERSION }}
+          </a></span>
           <v-spacer/>
           <span>NUC.RED</span>
         </v-footer>
@@ -27,6 +32,10 @@ export default {
   name: 'App',
   components: {
     NavBar
-  }
+  },
+  data: () => ({
+    // eslint-disable-next-line no-undef
+    versionInfo: VER_INFO
+  })
 };
 </script>
